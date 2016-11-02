787df7c6c79a2709cd166a2247df4c0b00d6e06c
Volodymyr Derecha <volodymyrderecha@gmail.com>
2016-11-02 11:35:05 +0200

    Integrate redis client and test emitter to socket io project

--- server/src/app.ts
+++ server/src/app.ts
@@ -29,23 +29,45 @@ class Server {
 
         this.sockets();
 
+        this.demoNotificationEmitter();
+
         this.listen();
     }
 
     private config(): void {
         this.port = process.env.PORT || 5000;
 
+        this.root = path.join(path.resolve(__dirname, '../dist'));
     }
 
     private routes(): void {
         let router: express.Router;
         router = express.Router();
+
+        router.get('/', (request: express.Request, result: express.Response) => {
+            result.sendFile(path.join(this.root, '/index.html'));
+        });
+
         this.app.use('*', router);
     }
 
-    private sockets(): void {
-        this.io = socketIo(this.server);
+    private demoNotificationEmitter(): void {
+        const io = require('socket.io-emitter')({ host: 'localhost', port: 6379 });
+        const socket = io.of('/notifications');
+        setInterval(() => {
+            console.log('start notification');
+            socket.emit('notification', { message: 'time: ' + Date.now(), username: 'test' });
+        }, 5000);
+    }
 
+    private sockets(): void {
+        const redis = require('redis');
+        const redisAdapter = require('socket.io-redis');
+        const sub = redis.createClient(6379, 'localhost', { return_buffers: true });
+        const adapter = redisAdapter({ subClient: sub });
+        this.io = socketIo(this.server, {
+            adapter: adapter
+        });
         let notificationSocket = new NotificationSocket(this.io);
     }
 
