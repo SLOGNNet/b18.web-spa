b3e452dffc5d2caf7047cc7c41a8fe3aef76e4e3
Andrii Melnyk <andrew.melnyk@bridge18.com>
2016-11-24 13:30:36 +0200

    FSPA-84 #comment Added new markup and functiolity

--- src/app/common/bd-input/bd-input.component.ts
+++ src/app/common/bd-input/bd-input.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, Input, EventEmitter, HostBinding, forwardRef } from '@angular/core';
 const noop = () => { };
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
@@ -17,27 +16,24 @@ export const BD_INPUT_CONTROL_VALUE_ACCESSOR: any = {
 })
 
 
-export class  CommonInputComponent {
-    private state:any[] = [];
-    private stateClasses:string = '';
-    private _onTouchedCallback: () => void = noop;
-    private _onChangeCallback: (_: any) => void = noop;
-    private _value: string = '';
-    private _empty: boolean = true;
-    private _bdFocused: string = '';
-    private _isAnimated: string = '';
-    private _isInvalid: boolean = false;
-    private _invalidClass: string = '';
-    private _focused: boolean = false;
-    private _disabled: boolean = false;
-    private _blurEmitter: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
-    private _focusEmitter: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
-    private  coerceBooleanProperty( value: any): boolean {
-      return value != null && `${value}` !== 'false';
-    }
-    private setInvalidState(val: boolean): void {
-      val ? this._invalidClass = 'invalid' : this._invalidClass = '';
-    }
+export class CommonInputComponent {
+  private state: any[] = [];
+  private stateClasses: string = '';
+  private _onTouchedCallback: () => void = noop;
+  private _onChangeCallback: (_: any) => void = noop;
+  private _value: string = '';
+  private _empty: boolean = true;
+  private _isInvalid: boolean = false;
+  private _isDefaultView: boolean = true;
+  private _focused: boolean = false;
+  private _disabled: boolean = false;
+  private _blurEmitter: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+  private _focusEmitter: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+
+
+  private coerceBooleanProperty(value: any): boolean {
+    return value != null && `${value}` !== 'false';
+  }
 
   private _convertValueForInputType(v: any): any {
     switch (this.type) {
@@ -46,24 +42,25 @@ export class  CommonInputComponent {
     }
   }
 
-    get focused() { return this._focused; }
-    get empty() { return (this._value == null || this._value === '') && this.type !== 'date '; }
-    get characterCount(): number {
-        return this.empty ? 0 : ('' + this._value).length;
-    }
-    @Input() errorText: string = '';
-    @Input() labelText: any;
-    @Input() animated: boolean;
-    @Input() name: string = null;
-    @Input() type: string = 'text';
-    @Input() set invalid(v: any) {
-      this._isInvalid = v;
-      this.setInvalidState(this._isInvalid);
-    }
-    @Input() get disabled(): boolean {
-      return this._disabled;
-    }
-    @Input() set value(v: any) {
+  get focused() { return this._focused; }
+  get characterCount(): number {
+    return this._empty ? 0 : ('' + this._value).length;
+  }
+  @Input() errorText: string = '';
+  @Input() labelText: any;
+  @Input() set defaultView(v: any) {
+    this._isDefaultView = v;
+    this.updateState();
+  };
+  @Input() name: string = null;
+  @Input() type: string = 'text';
+  @Input() set invalid(v: any) {
+    this._isInvalid = v;
+  }
+  @Input() get disabled(): boolean {
+    return this._disabled;
+  }
+  @Input() set value(v: any) {
     v = this._convertValueForInputType(v);
     if (v !== this._value) {
       this._value = v;
@@ -74,22 +71,17 @@ export class  CommonInputComponent {
     return this._value;
   };
 
-    _handleFocus(event: FocusEvent) {  
-      this.addClassIntoState('bd-focused');
-      console.log(this._bdFocused);
-      this.Animated(this.animated);
-      this._focused = true;
-      this._focusEmitter.emit(event);
-      this.updateState();
-    }
+  _handleFocus(event: FocusEvent) {
+    if (!this.state.includes('bd-static')) this.addClassIntoState('bd-focused');
+    this._focused = true;
+    this._focusEmitter.emit(event);
+    this.updateState();
+  }
 
-    set disabled(value) {
+  set disabled(value) {
     this._disabled = this.coerceBooleanProperty(value);
-    }
+  }
 
-    Animated(animated:boolean): void{
-      animated ? this._isAnimated = 'addAnimation' : this._isAnimated = '';
-    }
 
   _handleChange(event: Event) {
     this.value = (<HTMLInputElement>event.target).value;
@@ -97,37 +89,35 @@ export class  CommonInputComponent {
   }
 
   _handleBlur(event: FocusEvent) {
-    if(this._empty) this.removeClassFromState('bd-focused');
+    this.removeClassFromState('bd-focused');
     this.value !== '' ? this._empty = false : this._empty = true;
-    this.setInvalidState(this._isInvalid);
+    this._empty ? this.removeClassFromState('bd-static') : this.addClassIntoState('bd-static');
     this._focused = false;
     this._onTouchedCallback();
     this._blurEmitter.emit(event);
     this.updateState();
   }
 
-removeClassFromState(cl:any): void {
- this.state = this.state.filter(item => item !== cl);
-}
+  removeClassFromState(cl: any): void {
+    this.state = this.state.filter(item => item !== cl);
+  }
 
-addClassIntoState(cl:any): void {
- if(!this.state.includes(cl)) this.state.push(cl);
-}
+  addClassIntoState(cl: any): void {
+    if (!this.state.includes(cl)) this.state.push(cl);
+  }
 
 
-updateState(): void {
-  console.log(this._isInvalid);
- if(this._isInvalid) {this.addClassIntoState('bd-invalid');}
- else {
-   this.removeClassFromState('bd-invalid');
- }
- this.ConvertStateToString(this.state);
-}
+  updateState(): void {
+    this._isInvalid ? this.addClassIntoState('bd-invalid') : this.removeClassFromState('bd-invalid');
+    if (this._isDefaultView === false) this.addClassIntoState('bd-static');
 
-ConvertStateToString(arr: any[]){
+    // convert current state to classes string
+    this.ConvertStateToString(this.state);
+  }
+
+  ConvertStateToString(arr: any[]) {
     this.stateClasses = arr.map(item => item).join(' ');
-    console.log(this.stateClasses,'new Arr')
-}
+  }
 
   /**
    * Implemented as part of ControlValueAccessor.
