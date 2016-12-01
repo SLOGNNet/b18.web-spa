2e00890aca54aeffd643341863a7fd5105525492
Volodymyr Derecha <volodymyrderecha@gmail.com>
2016-12-01 13:04:11 +0200

    fspa-90 #comment move common components declarations to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { NotificationService } from './notification.service';
 import { SocketService } from './socket.service';
+import { CommonModule } from '@angular/common';
+
+import { TypeaheadModule } from '../common/typeahead/typeahead.module.ts';
+import { BdTypeaheadComponent } from '../common/bd-typeahead';
+import { BdDropdownComponent } from '../common/bd-dropdown';
+import { BdFormButtonComponent } from '../common/bd-form-button';
+import { BdTextareaAutosize } from '../common/bd-input/autosize.directive';
+import { BdInputComponent } from '../common/bd-input';
+import { DropdownModule } from 'ng2-bootstrap/components/dropdown';
 
 @NgModule({
-    providers: [
-        NotificationService,
-        SocketService
-    ]
+  providers: [
+    NotificationService,
+    SocketService
+  ],
+  declarations: [
+    BdTypeaheadComponent,
+    BdFormButtonComponent,
+    BdDropdownComponent,
+    BdTextareaAutosize,
+    BdInputComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TypeaheadModule,
+    DropdownModule
+  ],
+  exports: [
+    BdTypeaheadComponent,
+    BdFormButtonComponent,
+    BdTextareaAutosize,
+    BdInputComponent,
+    BdDropdownComponent,
+    TypeaheadModule,
+    DropdownModule,
+    CommonModule,
+    FormsModule
+  ]
 })
-export class SharedModule {}
+export class SharedModule { }
