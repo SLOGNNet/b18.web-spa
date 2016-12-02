e02678a1dff1a07dd593734b110708c187b73b83
Volodymyr Derecha <volodymyrderecha@gmail.com>
2016-12-02 17:58:49 +0200

    FSPA-90 lint fixes

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CustomerForm } from './customer-form';
-import { AddressForm } from './address-form'
+import { AddressForm } from './address-form';
 import { SharedModule } from '../shared/shared.module';
 import { BdFormTypeaheadComponent } from '../common/bd-form-typeahead';
 import { BdLoadFormComponent } from './load-form';
