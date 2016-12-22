11fd5662355e92fbf8539b07dcdcbe324505e6f1
Volodymyr Derecha <volodymyrderecha@gmail.com>
2016-12-22 16:36:17 +0200

    FSPA-151 extract mock data to separate file

--- src/app/models/stop.ts
+++ src/app/models/stop.ts
@@ -1,5 +1,15 @@
-import { Commodity } from './index';
+import { Commodity, Address } from './index';
 
 export class Stop {
+  address: Address;
   commodities: Array<Commodity>;
+  date: string = '';
+  notes: string = '';
+
+  static create(): Stop{
+    const result = new Stop();
+    result.address = Address.create();
+    result.commodities = new Array<Commodity>();
+    return result;
+  }
 }
