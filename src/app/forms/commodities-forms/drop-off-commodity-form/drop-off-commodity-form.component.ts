4d5fc66d780ea8756f431fd8e962bd48c5de1dce
Volodymyr Derecha <volodymyrderecha@gmail.com>
2016-12-26 11:10:38 +0200

    FSPA-152 implement selecting value from pickup

--- src/app/forms/commodities-forms/drop-off-commodity-form/drop-off-commodity-form.component.ts
+++ src/app/forms/commodities-forms/drop-off-commodity-form/drop-off-commodity-form.component.ts
@@ -19,9 +19,11 @@ export class DropOffCommodityFormComponent extends BaseForm {
   private showPickups: boolean = false;
   get pickupCommodities() {
     const loadForm =  <FormGroup>this.formArray.root;
-    const pickups = loadForm.controls['pickups'].value[0];
-
-    return pickups && this.showPickups ? pickups.commodities : [];
+    const pickupCommodity = <FormGroup>(loadForm.controls['pickups']).controls[0];
+    const commodities = pickupCommodity && this.showPickups ? pickupCommodity.controls['commodities'].value : [];
+    const selected = this.commodityFormElement.renderFormData;
+    const result =  commodities.filter((c) => !selected.find(rd => rd.item === c));
+    return result;
   }
 
   onShowPickups() {
