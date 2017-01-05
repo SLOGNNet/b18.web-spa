4a4a36b40fdc71a9ea0dbb67817ef62c78dcbc10
Andrii Melnyk <andrew.melnyk@bridge18.com>
2017-01-05 13:24:07 +0200

    FSPA-188 #comment fixed card styles, load stops component, lint err

--- src/app/loads/components/card/card.component.ts
+++ src/app/loads/components/card/card.component.ts
@@ -2,10 +2,10 @@ import { Component, Input } from '@angular/core';
 import { Load } from '../../../models';
 
 @Component({
-    selector: 'card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.scss']
+  selector: 'card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  @Input()loads: Load;
+  @Input() load: Load;
 }
