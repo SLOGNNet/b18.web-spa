607a5fb161deab419f731efeb3684971926d81e1
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-17 12:47:13 +0200

    upload component refactoring

--- src/app/shared/components/bd-upload-file/bd-upload-file.component.ts
+++ src/app/shared/components/bd-upload-file/bd-upload-file.component.ts
@@ -11,26 +11,12 @@ const URL = 'http://localhost:5000/upload',
 })
 export class BdUploadFileComponent {
 
-      private _progressWidth: number = 0;
       private dragging: boolean = false;
-      private loading: boolean = false;
       private documentIssueDate: string;
       private documentFiles: any[] = [];
 
       get showProgressBar() {
         return this.dragging;
-      }
-
-      get uploadingProcess() {
-        return this.loading && !this.dragging;
-      }
-
-      get progressWidth(): number {
-          return DEFAULT_WIDTH - this._progressWidth;
-      }
-
-      set progressWidth(val: number) {
-         this._progressWidth = val;
       }
 
       @Input() private documentType: string = '';
@@ -62,39 +48,5 @@ export class BdUploadFileComponent {
             documents: this.documentFiles,
             type: this.documentType
           });
-
-          this.upload(fileList);
       }
-
-      public upload (files: File[]){
-            let formData: FormData = new FormData(),
-                  xhr: XMLHttpRequest = new XMLHttpRequest();
-            for (let i = 0; i < files.length; i++) {
-                formData.append('filename', files[i]);
-            }
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        console.log('successfully loaded)');
-                    } else {
-                        console.log('something went wrong(');
-                    }
-                }
-                this.progressWidth = 0;
-            };
-
-            xhr.upload.onprogress = (event) => {
-                this.loading = true;
-                this.progressWidth = Math.round(event.loaded / event.total * 100);
-            };
-
-            xhr.upload.onloadend = (event) => {
-                this.loading = false;
-            };
-
-            xhr.open('POST', URL, true);
-            xhr.setRequestHeader('enctype', 'multipart/form-data');
-            xhr.send(formData);
-    }
-
 }
