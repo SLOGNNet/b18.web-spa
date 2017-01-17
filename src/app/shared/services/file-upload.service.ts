85c47097322cb600968f54cc69f063ef90c0114b
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-17 12:44:49 +0200

    FSPA-193 #comment add service for uploading files

---
+++ src/app/shared/services/file-upload.service.ts
@@ -0,0 +1,59 @@
+import { Component, Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/share';
+
+const URL = 'http://localhost:5000/upload';
+
+@Injectable()
+export class FileUploadService {
+
+  private progress$: Observable<number>;
+  private progress: number = 0;
+  private progressObserver: any;
+  private xhr: XMLHttpRequest = new XMLHttpRequest();
+
+  constructor() {
+    this.progress$ = new Observable(observer => {
+      this.progressObserver = observer
+    });
+  }
+
+  public getObserver(): Observable<number> {
+    return this.progress$;
+  }
+
+  public upload(file: File) {
+    let formData: FormData = new FormData();
+
+    formData.append('filename', file);
+
+    this.xhr.onreadystatechange = () => {
+      if (this.xhr.readyState === 4) {
+        if (this.xhr.status === 200) {
+          console.log('successfully loaded)');
+        } else {
+          console.log('something went wrong(');
+        }
+      }
+    };
+
+    this.setUploadUpdateInterval(500);
+
+    this.xhr.upload.onprogress = (event) => {
+      this.progress = Math.round(event.loaded / event.total * 100);
+      this.progressObserver.next(this.progress);
+    };
+
+    this.xhr.open('POST', URL, true);
+    this.xhr.setRequestHeader('enctype', 'multipart/form-data');
+    this.xhr.send(formData);
+  }
+
+  public abort(){
+    this.xhr.abort();
+  }
+
+  public setUploadUpdateInterval(interval: number): void {
+    setInterval(() => { }, interval);
+  }
+}
