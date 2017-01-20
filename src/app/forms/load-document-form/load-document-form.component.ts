f0002e1378317a6e89682e6c4c19237bfe350729
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-20 12:18:25 +0200

    add file upload service to load document form, add async-pipe to stream progress into preview

--- src/app/forms/load-document-form/load-document-form.component.ts
+++ src/app/forms/load-document-form/load-document-form.component.ts
@@ -1,37 +1,51 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 import { Validators, FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { EnumHelperService, BdFormBuilder, BdFormGroup, FormValidationService } from '../../shared';
 import { ViewMode } from '../../shared/enums';
 import { Document } from '../../models';
 import { BaseForm } from '../base-form';
+import { FileUploadService } from '../../shared';
+import { Subject } from 'rxjs/Subject';
 import { without } from 'lodash';
+
+const URL = 'http://localhost:5000/upload';
 
 @Component(Object.assign({
   selector: 'load-document-form',
-  templateUrl: './load-document-form.component.html'
+  templateUrl: './load-document-form.component.html',
+  providers: [FileUploadService]
 }, BaseForm.metaData))
 export class LoadDocumentFormComponent extends BaseForm {
 
   @Input() items: Array<Document> = new Array<Document>();
-  private documents: Object[];
+  private documents: any[];
 
-  constructor(element: ElementRef) {
+  constructor(public fileUploadService: FileUploadService, element: ElementRef) {
     super(element);
   }
 
-  ngOnInit(){
-    this.documents = this.items;
-  }
-
-  ngOnChanges(){
+  ngOnChanges() {
     this.documents = this.items;
   }
 
   onLoadDocuments(event) {
-    event.documents.map(item => this.documents.push({ file: item, type: event.type, url: '', issueDate: '', newDocument: event.newDocument }));
+    event.documents.map(item => {
+      this.documents.push({
+        file: item,
+        type: event.type,
+        url: '',
+        issueDate: '',
+        newDocument: event.newDocument,
+        progress: this.fileUploadService.upload(URL, item)
+      });
+    });
   }
 
   onRemoveFile(document) {
+    let itemIndexToDelete = this.documents.indexOf(document);
+    if(this.documents[itemIndexToDelete].progress) {
+          this.documents[itemIndexToDelete].progress.subscribe().unsubscribe();
+    }
     this.documents = without(this.documents, document);
   }
 }
