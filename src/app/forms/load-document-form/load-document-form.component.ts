5ad809058f5573e2fcffc5d031b0b24e7f6bd98c
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-20 16:52:38 +0200

    refactoring of document an progress types

--- src/app/forms/load-document-form/load-document-form.component.ts
+++ src/app/forms/load-document-form/load-document-form.component.ts
@@ -6,9 +6,17 @@ import { Document } from '../../models';
 import { BaseForm } from '../base-form';
 import { FileUploadService } from '../../shared';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 import { without } from 'lodash';
 
 const URL = 'http://localhost:5000/upload';
+
+class ProgressItem {
+   progress: Observable<any>;
+   item: Document;
+
+   constructor(){}
+}
 
 @Component(Object.assign({
   selector: 'load-document-form',
@@ -18,25 +26,35 @@ const URL = 'http://localhost:5000/upload';
 export class LoadDocumentFormComponent extends BaseForm {
 
   @Input() items: Array<Document> = new Array<Document>();
-  private documents: any[];
+  private documents: Array<ProgressItem> = [];
 
   constructor(public fileUploadService: FileUploadService, element: ElementRef) {
     super(element);
   }
 
+  createProgressItem(progress, item): ProgressItem {
+    let result = new ProgressItem();
+    result.progress = progress;
+    result.item = item;
+    return result;
+  }
+
+  createDocumentItem(file, type): Document {
+    let result = new Document();
+    result.file = file;
+    result.type = type;
+    return result;
+  }
+
   ngOnChanges() {
-    this.documents = this.items;
+    this.items.map(item => {
+        this.documents.push(this.createProgressItem(null, this.createDocumentItem(item.file, item.type)));
+    });
   }
 
   onLoadDocuments(event) {
     event.documents.map(item => {
-      this.documents.push({
-        file: item,
-        type: event.type,
-        url: '',
-        issueDate: '',
-        progress: this.fileUploadService.upload(URL, item)
-      });
+      this.documents.push(this.createProgressItem(this.fileUploadService.upload(URL, item), this.createDocumentItem(item, event.type)));
     });
   }
 
