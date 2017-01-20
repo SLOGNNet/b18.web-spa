c0cf7b0b0534a0c7b51439d5925109c814fca6d8
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-20 16:51:06 +0200

    refactoring: remove unused flag

--- src/app/shared/components/bd-upload-file/bd-upload-file.component.ts
+++ src/app/shared/components/bd-upload-file/bd-upload-file.component.ts
@@ -40,7 +40,6 @@ export class BdUploadFileComponent {
           this.documentsSelected.emit({
             documents: this.documentFiles,
             type: this.documentType,
-            newDocument: true
           });
       }
 }
