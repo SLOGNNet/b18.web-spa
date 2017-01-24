c191d207ca0034ddb2594e6fcd370a1e3e2a1401
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-24 18:56:03 +0200

    add icons components to separate folder in shared module, refactor iconwithcountindicator component

---
+++ src/app/shared/components/bd-icons/icon-with-count-indicator/icon-with-count-indicator.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input, HostListener, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'icon-with-count-indicator',
+  templateUrl: './icon-with-count-indicator.component.html',
+  styleUrls: ['./icon-with-count-indicator.component.scss']
+})
+export class IconWithCountIndicatorComponent {
+  @Input() class: string;
+  @Input() count: number;
+  @Input() maxValue: number = 999;
+
+  @Input() private isActive: boolean = false;
+
+  constructor(private _eref: ElementRef){}
+
+  ngOnInit() {
+    this.updateCount();
+  }
+
+  updateCount() {
+    if (this.count > 0) {
+      if (this.count > this.maxValue) {
+        this.count = this.maxValue;
+      }
+    } else {
+      this.count = null;
+    }
+  }
+
+  ngOnChanges(changes) {
+    if (changes.count) {
+      this.updateCount();
+    }
+  }
+
+  toggleActive(){
+    this.isActive = !this.isActive;
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event): void {
+      this.toggleActive();
+  }
+
+  @HostListener('window:click')
+  onWindowClick(): void {
+      if(this._eref.nativeElement.contains(event.target)) return;
+      this.isActive = false;
+  }
+
+  @HostListener('mousemleave')
+  onMouseLeave(): void {
+      console.log("leaved");
+  }
+
+  @HostListener('window:mousemove',['$event'])
+  onWindowMouseMove(event): void {
+      //console.log("moved ", event.target);
+  }
+
+
+
+}
