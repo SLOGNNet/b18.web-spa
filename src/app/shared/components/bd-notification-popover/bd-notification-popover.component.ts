5eca459f9089200f295a3bd01ff04884e019a345
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-01-30 15:17:37 +0200

    add viewed param to notifications collection( current progress )

--- src/app/shared/components/bd-notification-popover/bd-notification-popover.component.ts
+++ src/app/shared/components/bd-notification-popover/bd-notification-popover.component.ts
@@ -3,6 +3,15 @@ import { BdMessageCardComponent, BdTaskCardComponent } from './notification-card
 import { BdPopoverContent } from './directives/bd-popover';
 import { Notification, NotificationType } from '../../../models';
 
+const POPOVER_LIMIT = 8;
+
+class ViewedNotification {
+   notification: Notification;
+   viewed: boolean;
+
+   constructor(){}
+}
+
 @Component({
     selector: 'bd-notification-popover',
     templateUrl: './bd-notification-popover.component.html',
@@ -15,7 +24,13 @@ export class BdNotificationPopoverComponent {
 
   @Input() itemsCount: number = 0;
   @Input() width: number;
-  @Input() items: Array<Notification> = [];
+  @Input() set items(notifications: Array<Notification>) {
+    this._items = notifications;
+    notifications.map(item => {
+      console.log(item);
+    });
+  }
+
   @Input() set notificationType(type: NotificationType) {
     const typeText = Notification.getTypeText(type);
     this.topIconClassName = this._itemsName = this.titleText = typeText;
@@ -23,6 +38,7 @@ export class BdNotificationPopoverComponent {
   }
 
   private notificationTypeEnum = NotificationType;
+  private _items: Array<Notification> = new Array<Notification>(POPOVER_LIMIT);
   private _iconClass: string;
   private _titleText: string;
   private _topIconActive: boolean = false;
@@ -41,6 +57,17 @@ export class BdNotificationPopoverComponent {
   set itemsName(val: string) {
     if (this.itemsCount > 1) this._itemsName = val + 's';
     else this._itemsName = val;
+  }
+
+  ngOnInit(){
+    console.log(this._items);
+  }
+
+  createSeenNotification(notification, viewed): ViewedNotification {
+    let result: ViewedNotification;
+    result.notification = notification;
+    result.viewed = viewed;
+    return result;
   }
 
   onRefreshClick(event) {
