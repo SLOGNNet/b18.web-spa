ffde323ca878356b4cd2c35bd0d204ef53f35654
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-02-01 16:25:15 +0200

    remove unused import from component

--- src/app/shared/components/bd-perfect-scrollbar/bd-perfect-scrollbar.component.ts
+++ src/app/shared/components/bd-perfect-scrollbar/bd-perfect-scrollbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import { Component, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
