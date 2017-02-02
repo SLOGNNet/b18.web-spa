218de7c168d5c5159f1f9daf703640c899e83e0a
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-02-02 14:32:42 +0200

    add active state to header filters name

--- src/app/shared/components/filter-container/filter-container.component.ts
+++ src/app/shared/components/filter-container/filter-container.component.ts
@@ -11,14 +11,18 @@ export class FilterContainer {
 
 @ContentChildren(BaseFilter) filters: QueryList<BaseFilter>;
 
-
   ngAfterContentInit() {
     if (this.filters[0]) {
       this.filters[0].active = true;
     }
   }
-  private _showFilter(filter: BaseFilter) {
 
+  private _toggleFilter(filter: BaseFilter) {
+    this.deactivateAllFilters();
     filter.active = true;
+  }
+
+  deactivateAllFilters() {
+    this.filters.forEach(f => f.active = false);
   }
 }
