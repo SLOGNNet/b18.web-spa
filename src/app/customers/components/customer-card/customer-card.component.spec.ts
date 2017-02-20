ce22d1b91d2621c634a2c0c397bb49ec9dc59229
Andrii Melnyk <andrew.melnyk@bridge18.com>
2017-02-20 20:34:41 +0200

    FSPA-332 #comment added part of tests

--- src/app/customers/components/customer-card/customer-card.component.spec.ts
+++ src/app/customers/components/customer-card/customer-card.component.spec.ts
@@ -1,17 +1,87 @@
-import { TestBed, ComponentFixture, fakeAsync, tick, async } from '@angular/core/testing';
+import { async, TestBed, ComponentFixture, inject } from '@angular/core/testing';
+import { createGenericTestComponent } from '../../../shared/test/common';
 import { By } from '@angular/platform-browser';
-import { DebugElement, Input, Output, EventEmitter, Directive } from '@angular/core';
+import { Component } from '@angular/core';
 import { CustomerCardComponent } from './index';
+import { SharedModule } from '../../../shared/shared.module';
+import { Load, Customer, Address, Contact, CustomerStatuses, CustomerTypes, LoadStatuses } from '../../../models';
 
 
-describe('customer-card', () => {
 
-  let component: CustomerCardComponent;
-  let fixture: ComponentFixture<CustomerCardComponent>;
-  let de: DebugElement;
+const mockCustomer = new Customer(),
+  testContact = new Contact(),
+  testCustomer = new Customer(),
+  testLoad = new Load(),
+  testAddress = new Address();
 
-  it('should be defined', () => {
+// test address
+  testAddress.phone = '(925) 937-8500',
+
+  // test contact
+  testContact.firstName = 'Emma',
+  testContact.lastName = 'Watson',
+  testContact.position = 'Sales manager',
+  // test load
+  testLoad.systemLoadNumber = 209282402,
+  testLoad.status = LoadStatuses.Booked,
+  // test customer
+  mockCustomer.name = 'CH ROBINSON COMPANY INC',
+  mockCustomer.addresses = [testAddress],
+  mockCustomer.contacts = [testContact],
+  mockCustomer.email = 'carrier.services@chrobinson.com',
+  mockCustomer.status = CustomerStatuses.Active,
+  mockCustomer.mc = '384859',
+  mockCustomer.loads = [testLoad];
+
+
+fdescribe('CustomerCardComponent', () => {
+  let fixture: ComponentFixture<CustomerCardComponent>,
+    component: CustomerCardComponent,
+    testCustomer: Customer = mockCustomer;
+
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CustomerCardComponent
+      ],
+      imports: [
+        SharedModule
+      ]
+    });
+    fixture = TestBed.createComponent(CustomerCardComponent);
+    component = fixture.componentInstance;
+    //
+
+  }));
+
+  it('should have a component instance', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should display customer name', () => {
+    component.customer = testCustomer;
+    fixture.detectChanges();
+    let element = fixture.debugElement.query(By.css('.customer-name'));
+    expect(element.nativeElement.textContent).toMatch('CH ROBINSON COMPANY INC');
+  });
+
+  it('should display customer contact position', () => {
+    component.customer = testCustomer;
+    fixture.detectChanges();
+    let element = fixture.debugElement.query(By.css('.position'));
+    expect(element.nativeElement.textContent).toMatch('Sales manager');
+  });
+
+  it('should handle click', async(() => {
+    component.customer = testCustomer;
+    spyOn(component, 'onClick');
+
+    let element = fixture.debugElement.query(By.css('.customer-card-section'));
+    console.log(component.customer);
+    element.nativeElement.click();
+
+    fixture.whenStable().then(() => {
+      expect(fixture.debugElement.componentInstance.onClick).toHaveBeenCalled();
+    });
+  }));
 });
