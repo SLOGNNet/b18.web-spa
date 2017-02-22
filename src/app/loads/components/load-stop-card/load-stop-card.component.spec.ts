7059e92b2ba6dc9d1d3240e4d4a8c8d6d1719589
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-02-22 16:11:13 +0200

    refactor load cars tests

--- src/app/loads/components/load-stop-card/load-stop-card.component.spec.ts
+++ src/app/loads/components/load-stop-card/load-stop-card.component.spec.ts
@@ -1,73 +1,74 @@
-import { async, TestBed, ComponentFixture, inject } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { LoadStopCardComponent } from '.';
 import { SharedModule } from '../../../shared/shared.module';
 import { CustomerPopoverComponent, DriverPopoverComponent, TripPopoverComponent } from './components';
 import { Load, LoadStatuses, Customer, Trip, Driver, Address, Equipment, Stop, StopTypes, StopStatuses, Facility } from '../../../models';
 
-const mockLoad = new Load();
-const testTrip = new Trip();
-const testDriver = new Driver();
-const testAddress = new Address();
-const testCustomer = new Customer();
-const testStop1 = new Stop();
-const testStop2 = new Stop();
-// test address
-testAddress.id = 1;
-testAddress.name = 'Main Office';
-testAddress.streetAddress = '14701 Charlson Road, United States';
-testAddress.city = 'Eden Prairie';
-testAddress.phone = '(925) 937-8500';
-testAddress.state = 'MN';
-testAddress.zip = '55347';
-testAddress.lat = 40.795675;
-testAddress.lng = -73.93600099999998;
-// test customer
-testCustomer.id = 1;
-testCustomer.mc = '384859';
-testCustomer.addresses = [testAddress, testAddress];
-testCustomer.name = 'CH ROBINSON COMPANY INC';
-testCustomer.contacts = [null];
-testCustomer.email = 'carrier.services@chrobinson.com';
-testCustomer.status = null;
-testCustomer.type = null;
-// test driver
-testDriver.id = 1;
-testDriver.firstName = 'John';
-testDriver.lastName = 'Doe';
-testDriver.powerUnitAssigned = Equipment.create();
-testDriver.trailerAssigned = Equipment.create();
-// test trip
-testTrip.id = 1;
-testTrip.number = 1212;
-testTrip.truckNumber = 1010;
-testTrip.trailerNumber = 1111;
-testTrip.driver = testDriver;
-// test stops
-testStop1.id = 1;
-testStop1.notes = 'notes';
-testStop1.type = StopTypes.Pickup;
-testStop1.address = testAddress;
-testStop1.date = new Date(2017, 0, 9);
-testStop1.facility = Facility.create();
-testStop1.status = StopStatuses.InProgress;
-testStop2.id = 2;
-testStop2.notes = 'notes';
-testStop2.type = StopTypes.Pickup;
-testStop2.address = testAddress;
-testStop2.date = new Date(2017, 1, 10);
-testStop2.facility = Facility.create();
-testStop2.status = StopStatuses.InProgress;
-// test load
-mockLoad.id = 1;
-mockLoad.customer = testCustomer;
-mockLoad.systemLoadNumber = 100500;
-mockLoad.customerLoadNumber = 500100;
-mockLoad.currentTrip = [testTrip];
-mockLoad.status = LoadStatuses.Completed;
-mockLoad.stops = [ testStop1, testStop2 ];
 
-describe('LoadStopCardComponent', () => {
+function createTestData() {
+  let resultLoad = new Load(),
+  testTrip = new Trip(),
+  testDriver = new Driver(),
+  testAddress = new Address(),
+  testCustomer = new Customer(),
+  testStop1 = new Stop(),
+  testStop2 = new Stop();
+  testAddress.id = 1;
+  testAddress.name = 'Main Office';
+  testAddress.streetAddress = '14701 Charlson Road, United States';
+  testAddress.city = 'Eden Prairie';
+  testAddress.phone = '(925) 937-8500';
+  testAddress.state = 'MN';
+  testAddress.zip = '55347';
+  testAddress.lat = 40.795675;
+  testAddress.lng = -73.93600099999998;
+  // test customer
+  testCustomer.id = 1;
+  testCustomer.mc = '384859';
+  testCustomer.addresses = [testAddress, testAddress];
+  testCustomer.name = 'CH ROBINSON COMPANY INC';
+  testCustomer.contacts = [null];
+  testCustomer.email = 'carrier.services@chrobinson.com';
+  testCustomer.status = null;
+  testCustomer.type = null;
+  // test driver
+  testDriver.id = 1;
+  testDriver.firstName = 'John';
+  testDriver.lastName = 'Doe';
+  testDriver.powerUnitAssigned = Equipment.create();
+  testDriver.trailerAssigned = Equipment.create();
+  // test trip
+  testTrip.id = 1;
+  testTrip.number = 1212;
+  testTrip.truckNumber = 1010;
+  testTrip.trailerNumber = 1111;
+  testTrip.driver = testDriver;
+  // test stops
+  testStop1.id = 1;
+  testStop1.notes = 'notes';
+  testStop1.type = StopTypes.Pickup;
+  testStop1.address = testAddress;
+  testStop1.date = new Date(2017, 0, 9);
+  testStop1.facility = Facility.create();
+  testStop1.status = StopStatuses.InProgress;
+  testStop2.id = 2;
+  testStop2.notes = 'notes';
+  testStop2.type = StopTypes.Pickup;
+  testStop2.address = testAddress;
+  testStop2.date = new Date(2017, 1, 10);
+  testStop2.facility = Facility.create();
+  testStop2.status = StopStatuses.InProgress;
+  // test load
+  resultLoad.id = 1;
+  resultLoad.customer = testCustomer;
+  resultLoad.currentTrip = [testTrip];
+  resultLoad.status = LoadStatuses.Completed;
+  resultLoad.stops = [ testStop1, testStop2 ];
+  return resultLoad;
+}
+
+fdescribe('LoadStopCardComponent', () => {
   let fixture: ComponentFixture<LoadStopCardComponent>,
       component: LoadStopCardComponent,
       testLoad: Load;
@@ -85,28 +86,34 @@ describe('LoadStopCardComponent', () => {
     ]});
     fixture = TestBed.createComponent(LoadStopCardComponent);
     component = fixture.componentInstance;
-    testLoad = mockLoad;
+    testLoad = createTestData();
   }));
 
   it('should have a component instance', () => {
+    console.log(testLoad);
     expect(component).toBeTruthy();
   });
 
   it('should display load number', () => {
+    let testLoadNumber = 100500;
     component.load = testLoad;
+    component.load.systemLoadNumber = testLoadNumber;
     fixture.detectChanges();
     let element = fixture.debugElement.query(By.css('.load-number'));
-    expect(element.nativeElement.textContent).toMatch('LD100500');
+    expect(element.nativeElement.textContent).toMatch('LD' + testLoadNumber);
   });
 
   it('should display customer load number', () => {
+    let testCustomerNumber = 100200;
     component.load = testLoad;
+    component.load.customerLoadNumber = testCustomerNumber;
     fixture.detectChanges();
     let element = fixture.debugElement.query(By.css('.customer-load-number'));
-    expect(element.nativeElement.textContent).toMatch('500100');
+    expect(element.nativeElement.textContent).toEqual(String(testCustomerNumber));
   });
 
   it('should display driver firstname', () => {
+    let testTripData = new Trip();
     component.load = testLoad;
     fixture.detectChanges();
     let element = fixture.debugElement.query(By.css('.firstName'));
@@ -129,6 +136,7 @@ describe('LoadStopCardComponent', () => {
 
   it('should display trip number', () => {
     component.load = testLoad;
+
     fixture.detectChanges();
     let element = fixture.debugElement.query(By.css('.tripNumber'));
     expect(element.nativeElement.textContent).toMatch('TR1212');
@@ -165,8 +173,15 @@ describe('LoadStopCardComponent', () => {
   it('should display stops line when stops length is greater than 1', () => {
     component.load = testLoad;
     fixture.detectChanges();
-    let element = fixture.debugElement.query(By.css('.right'));
-    expect(component.load.stops.length).toBeGreaterThan(1);
+    let element = fixture.debugElement.query(By.css('.stops-container'));
+    expect(element).toBeDefined();
+  });
+
+  it('should not display stops line when stops length is less than 1', () => {
+    component.load = testLoad;
+    fixture.detectChanges();
+    let element = fixture.debugElement.query(By.css('.stops-container'));
+    expect(element).toBeUndefined();
   });
 
   it('should display right address of first stop in load', () => {
@@ -180,7 +195,7 @@ describe('LoadStopCardComponent', () => {
     component.load = testLoad;
     fixture.detectChanges();
     let element = fixture.debugElement.query(By.css('.lastStopAddress'));
-    expect(element.nativeElement.textContent).toMatch('Eden Prairie, MN');
+    expect(element.nativeElement.textContent).toMatch(testLoad.stops[1].address.city + ', ' + testLoad.stops[1].address.state);
   });
 
   it('should handle click', () => {
@@ -188,43 +203,38 @@ describe('LoadStopCardComponent', () => {
     spyOn(component, 'onClick');
     let element = fixture.debugElement.query(By.css('.load-stop-card-section'));
     element.nativeElement.click();
-      fixture.whenStable().then(() => {
-            expect(fixture.debugElement.componentInstance.onClick).toHaveBeenCalled();
-      });
-    });
+    expect(fixture.debugElement.componentInstance.onClick).toHaveBeenCalled();
+  });
 
-    it('should send accurate data to CUSTOMER popover', () => {
+    it('should send customer\'s data to customer popover', () => {
       let addressData = new Address(),
           customerData = new Customer();
-      addressData.id = 1;
-      addressData.name = 'Main Office';
-      addressData.streetAddress = '14701 Charlson Road, United States';
-      addressData.city = 'Eden Prairie';
-      addressData.phone = '(925) 937-8500';
-      addressData.state = 'MN';
-      addressData.zip = '55347';
-      addressData.lat = 40.795675;
-      addressData.lng = -73.93600099999998;
-
-      customerData.id = 1;
-      customerData.mc = '384859';
-      customerData.addresses = [addressData, addressData];
-      customerData.name = 'CH ROBINSON COMPANY INC';
-      customerData.contacts = [null];
-      customerData.email = 'carrier.services@chrobinson.com';
-      customerData.status = null;
-      customerData.type = null;
+          addressData.id = 1;
+          addressData.name = 'Main Office';
+          addressData.streetAddress = '14701 Charlson Road, United States';
+          addressData.city = 'Eden Prairie';
+          addressData.phone = '(925) 937-8500';
+          addressData.state = 'MN';
+          addressData.zip = '55347';
+          addressData.lat = 40.795675;
+          addressData.lng = -73.93600099999998;
+          customerData.id = 1;
+          customerData.mc = '384859';
+          customerData.addresses = [addressData, addressData];
+          customerData.name = 'CH ROBINSON COMPANY INC';
+          customerData.contacts = [null];
+          customerData.email = 'carrier.services@chrobinson.com';
+          customerData.status = null;
+          customerData.type = null;
 
       component.load = testLoad;
+      component
       fixture.detectChanges();
       let element = fixture.debugElement.query(By.directive(CustomerPopoverComponent)).componentInstance;
-
-      fixture.whenStable().then(() => {
-          expect(element.customer).toEqual(customerData);
-      });
+      expect(element.customer).toEqual(customerData);
     });
 
-    it('should send accurate data to DRIVER popover', () => {
+    it('should send current trip\'s driver data to driver popover', () => {
       let driverData = new Driver();
       driverData.id = 1;
       driverData.firstName = 'John';
@@ -233,6 +243,7 @@ describe('LoadStopCardComponent', () => {
       driverData.trailerAssigned = Equipment.create();
 
       component.load = testLoad;
+      component.load.currentTrip[0].driver = driverData;
       fixture.detectChanges();
       let element = fixture.debugElement.query(By.directive(DriverPopoverComponent)).componentInstance;
       fixture.whenStable().then(() => {
@@ -241,7 +252,7 @@ describe('LoadStopCardComponent', () => {
     });
 
 
-    it('should send accurate data to TRIP popover', () => {
+    it('should send current trip data to trip popover', () => {
       let tripData = new Trip();
       tripData.id = 1;
       tripData.number = 1212;
@@ -255,11 +266,10 @@ describe('LoadStopCardComponent', () => {
       tripData.driver.trailerAssigned = Equipment.create();
 
       component.load = testLoad;
+      component.load.currentTrip = [];
+      component.load.currentTrip.push(tripData);
       fixture.detectChanges();
       let element = fixture.debugElement.query(By.directive(TripPopoverComponent)).componentInstance;
-      fixture.whenStable().then(() => {
-          expect(element.trip).toEqual(tripData);
-      });
+      expect(element.trip).toEqual(tripData);
     });
-
 });
