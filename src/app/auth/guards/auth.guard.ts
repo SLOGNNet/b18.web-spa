a10b0f09b57ed032623e50c8dde3bfdc0f6831d0
Mykhailo <mike.stakhiv@bridge18.com>
2017-02-23 17:41:27 +0200

    FSPA-332 revert app and auth guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -9,6 +9,12 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authenticationService: AuthenticationService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return true;
+    if (localStorage.getItem('currentUser')) {
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url 
+    this.router.navigate(['/login']);
+    return false;
   }
 }
