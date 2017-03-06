423cc5e3d35386ae55c8eb64584212ea3e7367a8
Andrii Melnyk <andrew.melnyk@bridge18.com>
2017-03-06 20:21:35 +0200

    FSPA-384 #comment code refactoring

--- src/app/shared/components/bd-checkboxes/bd-checkboxes.component.ts
+++ src/app/shared/components/bd-checkboxes/bd-checkboxes.component.ts
@@ -8,7 +8,7 @@ import { includes, without, some } from 'lodash';
 })
 export class BdCheckboxes {
   @Input() labelText: any;
-  @Input() disabled: any;
+  @Input() disabled: boolean;
   @Input() items: Array<any>;
   @Input() value: string = '';
   @Output() valueChange = new EventEmitter();
