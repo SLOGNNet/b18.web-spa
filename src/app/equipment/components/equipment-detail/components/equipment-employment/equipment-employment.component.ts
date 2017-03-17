1b826accbb46a63fca4ba2c6ac566f1d9b3b5153
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-03-17 13:51:21 +0200

    FSPA-453 edit employment component: add input data, refactor styles

--- src/app/equipment/components/equipment-detail/components/equipment-employment/equipment-employment.component.ts
+++ src/app/equipment/components/equipment-detail/components/equipment-employment/equipment-employment.component.ts
@@ -7,8 +7,21 @@ import { Equipment } from '../../../../../models';
   styleUrls: ['./equipment-employment.component.scss'],
 })
 export class EquipmentEmploymentComponent {
-  @Input() test: Equipment;
+  @Input() equipment: Equipment;
 
+  get equipmentOwnership() {
+    return Equipment.getModeText(this.equipment.ownership);
+  }
+
+  get equipmentType() {
+    return Equipment.getTypeText(this.equipment.type);
+  }
+
+  get equipmentSubType() {
+    return Equipment.getEquipmentSubType(this.equipment.type, this.equipment.subType);
+  }
+
+  get vehicleOperating() {
+    return Equipment.getVehicleOperatingsType(this.equipment.vehicleOperating);
+  }
 }
-
-
