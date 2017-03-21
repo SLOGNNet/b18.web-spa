7335e4cd3a78e9275aabe3316e1b65290912f15c
Volodymyr Derecha <volodymyrderecha@gmail.com>
2017-03-21 19:59:35 +0200

    add dev auth url

--- config/env/config.js
+++ config/env/config.js
@@ -21,7 +21,8 @@ config = {
   },
   publicdev: {
     socketIoHost: 'https://dev.bridge18.com',
-    apiUrl: 'https://dev.bridge18.com/v1/api/expedition/'
+    apiUrl: 'https://dev.bridge18.com/v1/api/expedition/',
+    authUrl: 'https://dev.bridge18.com/api/'
   },
   test: {
 
