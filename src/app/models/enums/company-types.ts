45d9414bb47a187af3e4c35e9ae267bb1d50567a
Andrii Melnyk <andrew.melnyk@bridge18.com>
2017-03-24 18:12:49 +0200

    FSPA-478 #comment fixed dropdowns issues

--- src/app/models/enums/company-types.ts
+++ src/app/models/enums/company-types.ts
@@ -7,7 +7,7 @@ export enum CompanyTypes {
 
 let displayTexts = {
   [CompanyTypes.BROKER]: 'Broker',
-  [CompanyTypes.FREIGHT_FORWARDER]: 'Freight Forwarder',
+  [CompanyTypes.FREIGHT_FORWARDER]: 'Freight_Forwarder',
   [CompanyTypes.SHIPPER]: 'Shipper',
   [CompanyTypes.CARRIER]: 'Carrier'
 };
