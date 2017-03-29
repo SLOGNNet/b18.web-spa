a8dee1fbae8c769b081771cedfc54765a27c8d73
Ruslan Kurtianyk <admin@ruslan-imac.local>
2017-03-29 16:25:12 +0300

    FSPA-494 fix issue with redux state mutation in when company update

--- src/app/store/reducers/company.reducer.ts
+++ src/app/store/reducers/company.reducer.ts
@@ -28,10 +28,11 @@ export const companyReducer = createReducer(INITIAL_STATE, {
     return Object.assign({}, state, { items: removeItem(state.companies, action.company)});
   },
   [CompanyActions.UPDATE_COMPANY_REQUEST](state, action) {
-    return Object.assign({}, state,
-      {
-        isLoading: true
-      });
+     return Object.assign({}, state, {
+      items: updateListItem(state.items, action.company),
+      selected: updateItem(state.selected, action.company),
+      isLoading: false
+    });
   },
   [CompanyActions.UPDATE_COMPANY_SUCCESS](state, action) {
     return Object.assign({}, state,
