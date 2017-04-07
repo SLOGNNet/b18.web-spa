348564c966d8759e6192927819f73d3a077c9e48
Andrii Melnyk <andrew.melnyk@bridge18.com>
2017-04-07 12:36:55 +0300

    FSPA-464 #commemnt code refactoring

--- src/app/base/base-card/base-card.component.ts
+++ src/app/base/base-card/base-card.component.ts
@@ -1,9 +1,13 @@
-import { Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter, HostBinding } from '@angular/core';
 
 export abstract class BaseCardComponent {
     @Input() item: any;
+    @Input() active: boolean;
     public statusText: boolean = false;
     @Output() select: EventEmitter<any> = new EventEmitter();
+    @HostBinding('class.active') get isActive() {
+      return this.active;
+    }
 
     protected abstract itemStatusText();
 
