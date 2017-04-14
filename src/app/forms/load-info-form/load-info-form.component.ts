406d5e2ddfda51730f1ff74cfa4cfa81c355b34e
Mykhailo <mike.stakhiv@bridge18.com>
2017-04-14 17:49:19 +0300

    FSPA-533 add additional field to load edit info

--- src/app/forms/load-info-form/load-info-form.component.ts
+++ src/app/forms/load-info-form/load-info-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnChanges, EventEmitter, ElementRef } from '@angular/core';
+import { Component, Input, Output, OnChanges, EventEmitter, ElementRef, ChangeDetectionStrategy } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs/Rx';
 import { CompanyService, ContactService } from '../../shared';
@@ -6,7 +6,8 @@ import { EnumHelperService } from '../../shared/helpers';
 import {
   Load, Document, Company,
   DriverRequirements, PowerUnitTypes, TrailerTypes,
-  Stop, StopTypes, LoadType, FreightType } from '../../models';
+  Stop, StopTypes, LoadType, FreightType, ReeferType
+} from '../../models';
 import { ViewMode } from '../../shared/enums';
 import { BaseForm } from '../base-form';
 import { StopActions } from '../../actions';
@@ -15,7 +16,8 @@ import { select } from '@angular-redux/store';
 @Component(Object.assign({
   selector: 'load-info-form',
   styleUrls: ['load-info-form.component.scss'],
-  templateUrl: './load-info-form.component.html'
+  templateUrl: './load-info-form.component.html',
+  changeDetectionStrategy: ChangeDetectionStrategy.OnPush
 }, BaseForm.metaData))
 export class LoadInfoFormComponent extends BaseForm implements OnChanges {
   driverRequirementsNames: Array<any>;
@@ -23,12 +25,26 @@ export class LoadInfoFormComponent extends BaseForm implements OnChanges {
   requiredTrailerTypesNames: Array<any>;
   loadTypesNames: Array<any>;
   freightTypesNames: Array<any>;
+  reeferTypesNames: Array<any>;
+  isReefer: boolean = true;
   @Input() public isLoading: boolean = false;
   @Input() load: Load;
   @Output() cancel: EventEmitter<any> = new EventEmitter();
   @Output() save: EventEmitter<any> = new EventEmitter();
   @Output() addCustomer: EventEmitter<any> = new EventEmitter();
-  public loadForm: FormGroup;
+  public loadForm: FormGroup = this.formBuilder.group({});
+  private fields = [
+    { name: 'id', validators: [] },
+    { name: 'customerLoadNo', validators: [] },
+    { name: 'driverRequirment', validators: [] },
+    { name: 'requiredPowerUnitType', validators: [] },
+    { name: 'requiredTrailerType', validators: [] },
+    { name: 'specialRequirments', validators: [] },
+    { name: 'type', validators: [] },
+    { name: 'freightType', validators: [] },
+    { name: 'reeferType', validators: [] },
+    { name: 'temperature', validators: [] },
+  ];
 
   public constructor(
     private companyService: CompanyService,
@@ -42,6 +58,18 @@ export class LoadInfoFormComponent extends BaseForm implements OnChanges {
     this.requiredTrailerTypesNames = this.enumHelperService.getDropdownKeyValues(TrailerTypes);
     this.loadTypesNames = this.enumHelperService.getDropdownKeyValues(LoadType);
     this.freightTypesNames = this.enumHelperService.getDropdownKeyValues(FreightType);
+    this.reeferTypesNames = this.enumHelperService.getDropdownKeyValues(ReeferType);
+    this.loadForm.valueChanges.subscribe(v => {
+      const isReefer = v.freightType === FreightType.REEFER;
+
+      if (isReefer !== this.isReefer) {
+        this.isReefer = isReefer;
+        if (!this.isReefer) {
+          this.loadForm.setControl('reeferType', this.formBuilder.control({ value: null, disabled: false }));
+          this.loadForm.setControl('temperature', this.formBuilder.control({ value: '', disabled: false }));
+        }
+      }
+    });
   }
 
   ngOnChanges(changes: any) {
@@ -51,15 +79,11 @@ export class LoadInfoFormComponent extends BaseForm implements OnChanges {
   }
 
   public initForm() {
-    this.loadForm = this.formBuilder.group({
-      id: [this.load.id],
-      customerLoadNo: [this.load.customerLoadNo],
-      driverRequirment: [this.load.driverRequirment],
-      requiredPowerUnitType: [this.load.requiredPowerUnitType],
-      requiredTrailerType: [this.load.requiredTrailerType],
-      specialRequirments: [this.load.specialRequirments],
-      type: [this.load.type],
-      freightType: [this.load.freightType]
+    this.fields.forEach(field => {
+      this.loadForm.setControl(
+        field.name,
+        this.formBuilder.control({ value: this.load[field.name], disabled: false }, field.validators)
+      );
     });
   }
 
